70fcf5ac7f69e1b40bbe92dee705d54cbead4f09
Faranheit15 <ffaranm15@gmail.com>
2021-07-03 23:38:32 +0530

    commit try 2

---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const projects = [{
+        title: "Konnect",
+        subtitle: "React | Firebase",
+        description: "Konnect is a text-messaging app, made using ReactJS and Firebase, which follows the concept of chatrooms",
+        image: "./project-2.gif",
+        link: "https://konnect-3f625.web.app/",
+    },
+    {
+        title: "Newsletter Sign-Up Page",
+        subtitle: "Node.js | MailChimp API",
+        description: "A newsletter  subscription web app for sending updates to your subscribers",
+        image: "./project-1.gif",
+        link: "https://faran-newsletter.herokuapp.com/",
+    },
+    {
+        title: "Weather App",
+        subtitle: "Node.js | OpenWeather API",
+        description: "A simple weather app backend to get the weather detauls of a city",
+        image: "./project-2.gif",
+        link: "https://weather-app-open.herokuapp.com",
+    },
+    {
+        title: "To Do List",
+        subtitle: "EJS | Mongoose",
+        description: "A simple To-Do List app",
+        image: "./project-4.gif",
+        link: "https://f-to-do-list.herokuapp.com/",
+    },
+    {
+        title: "Blog Website",
+        subtitle: "EJS | Mongoose",
+        description: "A simple website to write and publish blogs",
+        image: "./project-3.gif",
+        link: "https://fblog-website.herokuapp.com/",
+    },
+];
+
+export const testimonials = [{
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+        name: "Jesse Hicks",
+        company: "Zoozle",
+    },
+    {
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+        image: "https://randomuser.me/api/portraits/men/94.jpg",
+        name: "Ruben Alvarez",
+        company: "Dooble.io",
+    },
+];
+
+export const skills = [
+    "JavaScript",
+    "React",
+    "Python",
+    "Node",
+    "Flutter",
+    "HTML | CSS | Bootstrap",
+];
